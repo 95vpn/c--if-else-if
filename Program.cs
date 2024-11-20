2e64ae93fcaeb0d88e49f0ce282b5d4b0d49b218
Johnny Herrera <jmjaramillo95@gmail.com>
2024-11-20 14:49:57 -0500

    if-else-if-test

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random random = new Random();
+int daysUntilExpirations = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine(daysUntilExpirations);
+
+if (daysUntilExpirations == 0) {
+    Console.WriteLine("Your subscription ha expirado");
+}
+else if (daysUntilExpirations == 1) 
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if ( daysUntilExpirations <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpirations}");
+    discountPercentage = 10;
+}
+else if (daysUntilExpirations <= 10) 
+{
+    Console.WriteLine("Your subscription expires soon. Renew now");
+}
+
+if(discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
